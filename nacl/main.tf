7cd318815790d37b53a246de7c01ecb0dbb0885e
3rumalarao <3rumalarao@gmail.com>
2025-03-29 23:26:35 -0500

    Add files via upload

--- nacl/main.tf
+++ nacl/main.tf
@@ -1,1 +1,51 @@
+resource "aws_network_acl" "public" {
+  vpc_id = var.vpc_id
+  tags   = merge(var.tags, { Name = "public-nacl" })
 
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+}
+
+resource "aws_network_acl" "private" {
+  vpc_id = var.vpc_id
+  tags   = merge(var.tags, { Name = "private-nacl" })
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = var.allowed_cidr
+    from_port  = 0
+    to_port    = 65535
+  }
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+}
