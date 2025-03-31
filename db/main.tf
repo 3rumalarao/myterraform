8796a46c19bfb3b472598a82faeef1171585cd39
3rumalarao <3rumalarao@gmail.com>
2025-03-30 22:09:53 -0500

    Update main.tf

--- db/main.tf
+++ db/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "mysql" {
-  ami                   = var.mysql_ami
-  instance_type         = var.mysql_instance_type
-  subnet_id             = element(var.private_subnet_ids, 0)
-  vpc_security_group_ids = [var.db_sg_id]
+  ami           = var.mysql_ami
+  instance_type = element(var.mysql_instance_type_list, 0)
+  subnet_id     = element(var.private_subnet_ids, 0)
+  vpc_security_group_ids = var.mysql_sg_ids
   tags = merge(var.tags, {
     Name = "${var.name_prefix}-mysql",
     Role = "db-mysql"
@@ -10,10 +10,10 @@ resource "aws_instance" "mysql" {
 }
 
 resource "aws_instance" "postgresql" {
-  ami                   = var.postgres_ami
-  instance_type         = var.postgres_instance_type
-  subnet_id             = element(var.private_subnet_ids, 1)
-  vpc_security_group_ids = [var.db_sg_id]
+  ami           = var.postgres_ami
+  instance_type = var.postgres_instance_type
+  subnet_id     = element(var.private_subnet_ids, 1)
+  vpc_security_group_ids = var.postgres_sg_ids
   tags = merge(var.tags, {
     Name = "${var.name_prefix}-postgresql",
     Role = "db-postgresql"
