5afa707f06f3379986e4c58cd13c8094a7efd3cb
3rumalarao <3rumalarao@gmail.com>
2025-03-30 22:08:35 -0500

    Update variables.tf

--- public_server/variables.tf
+++ public_server/variables.tf
@@ -1,22 +1,41 @@
 variable "public_instance_count" {
-  type = number 
+  description = "Number of public server instances"
+  type        = number
+  default     = 2
 }
-variable "public_ami" {
-  type = string 
+
+variable "public_ami_list" {
+  description = "List of AMI IDs for public servers"
+  type        = list(string)
 }
-variable "public_instance_type" {
-  type = string 
+
+variable "public_instance_types" {
+  description = "List of instance types for public servers"
+  type        = list(string)
 }
+
+variable "public_instance_names" {
+  description = "List of instance names for public servers"
+  type        = list(string)
+}
+
+variable "public_sg_ids" {
+  description = "List (of lists) of security group IDs for each public instance"
+  type        = list(list(string))
+}
+
 variable "public_subnet_ids" {
-  type = list(string) 
+  description = "List of public subnet IDs"
+  type        = list(string)
 }
-variable "public_sg_id" {
-  type = string 
-}
+
 variable "name_prefix" {
-  type = string 
+  description = "Prefix for naming resources"
+  type        = string
 }
+
 variable "tags" {
-  type = map(string) 
-  default = {} 
+  description = "Tags to assign to resources"
+  type        = map(string)
+  default     = {}
 }
