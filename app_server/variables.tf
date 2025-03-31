b02c92ef8f1634e07d026081383b4db1e57ef7f2
3rumalarao <3rumalarao@gmail.com>
2025-03-30 22:06:26 -0500

    Update variables.tf

--- app_server/variables.tf
+++ app_server/variables.tf
@@ -1,22 +1,41 @@
 variable "app_instance_count" {
-  type = number 
+  description = "Number of application instances"
+  type        = number
+  default     = 2
 }
-variable "app_ami" {
-  type = string 
+
+variable "app_ami_list" {
+  description = "List of AMI IDs for application servers"
+  type        = list(string)
 }
-variable "app_instance_type" {
-  type = string 
+
+variable "app_instance_types" {
+  description = "List of instance types for application servers"
+  type        = list(string)
 }
+
+variable "app_instance_names" {
+  description = "List of instance names for application servers"
+  type        = list(string)
+}
+
+variable "app_sg_ids" {
+  description = "List (of lists) of security group IDs for each app instance"
+  type        = list(list(string))
+}
+
 variable "private_subnet_ids" {
-  type = list(string) 
+  description = "List of private subnet IDs"
+  type        = list(string)
 }
-variable "app_sg_id" {
-  type = string 
-}
+
 variable "name_prefix" {
-  type = string 
+  description = "Prefix for naming resources"
+  type        = string
 }
-variable "tags" { 
-  type = map(string)
-  default = {} 
+
+variable "tags" {
+  description = "Tags to assign to resources"
+  type        = map(string)
+  default     = {}
 }
