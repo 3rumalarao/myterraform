e61fcbec47e2541759c2263479c0a8daa9f72dc0
3rumalarao <3rumalarao@gmail.com>
2025-03-30 22:09:28 -0500

    Update variables.tf

--- db/variables.tf
+++ db/variables.tf
@@ -1,25 +1,45 @@
 variable "mysql_ami" {
-  type = string 
+  description = "AMI for MySQL instance"
+  type        = string
 }
-variable "mysql_instance_type" {
-  type = string 
+
+variable "mysql_instance_type_list" {
+  description = "List of instance types for MySQL (if more than one)"
+  type        = list(string)
 }
+
 variable "postgres_ami" {
-  type = string 
+  description = "AMI for PostgreSQL instance"
+  type        = string
 }
+
 variable "postgres_instance_type" {
-  type = string 
+  description = "Instance type for PostgreSQL instance"
+  type        = string
 }
+
+variable "mysql_sg_ids" {
+  description = "List of security group IDs for the MySQL instance"
+  type        = list(string)
+}
+
+variable "postgres_sg_ids" {
+  description = "List of security group IDs for the PostgreSQL instance"
+  type        = list(string)
+}
+
 variable "private_subnet_ids" {
-  type = list(string) 
+  description = "List of private subnet IDs"
+  type        = list(string)
 }
-variable "db_sg_id" {
-  type = string 
-}
+
 variable "name_prefix" {
-  type = string 
+  description = "Prefix for naming resources"
+  type        = string
 }
+
 variable "tags" {
-  type = map(string) 
-  default = {} 
+  description = "Tags to assign to resources"
+  type        = map(string)
+  default     = {}
 }
