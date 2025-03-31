ded78e33bf48fb9867d2ce2a9fa30efc5154355c
3rumalarao <3rumalarao@gmail.com>
2025-03-31 00:37:29 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,19 +9,3 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
-
-output "alb_dns_name" {
-  value = module.alb.alb_dns_name
-}
-
-output "efs_id" {
-  value = module.efs.efs_id
-}
-
-output "rds_mysql_endpoint" {
-  value = module.rds.rds_mysql_endpoint
-}
-
-output "backup_vault_name" {
-  value = module.backup.backup_vault_name
-}
