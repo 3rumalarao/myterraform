fee1f24d1d997519f5c8854809081940225f1954
3rumalarao <3rumalarao@gmail.com>
2025-03-30 22:08:56 -0500

    Update main.tf

--- public_server/main.tf
+++ public_server/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "public_server" {
-  count                 = var.public_instance_count
-  ami                   = var.public_ami
-  instance_type         = var.public_instance_type
-  subnet_id             = element(var.public_subnet_ids, count.index % length(var.public_subnet_ids))
-  vpc_security_group_ids = [var.public_sg_id]
+  count         = var.public_instance_count
+  ami           = element(var.public_ami_list, count.index % length(var.public_ami_list))
+  instance_type = element(var.public_instance_types, count.index % length(var.public_instance_types))
+  subnet_id     = element(var.public_subnet_ids, count.index % length(var.public_subnet_ids))
+  vpc_security_group_ids = element(var.public_sg_ids, count.index % length(var.public_sg_ids))
   tags = merge(var.tags, {
-    Name = "${var.name_prefix}-public-${count.index + 1}",
+    Name = element(var.public_instance_names, count.index),
     Role = "public"
   })
 }
