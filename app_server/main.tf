5105564d7bc9e83fc6b44ae6aba57e2e370e7801
3rumalarao <3rumalarao@gmail.com>
2025-03-30 22:07:01 -0500

    Update main.tf

--- app_server/main.tf
+++ app_server/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "app_server" {
-  count                 = var.app_instance_count
-  ami                   = var.app_ami
-  instance_type         = var.app_instance_type
-  subnet_id             = element(var.private_subnet_ids, count.index % length(var.private_subnet_ids))
-  vpc_security_group_ids = [var.app_sg_id]
+  count         = var.app_instance_count
+  ami           = element(var.app_ami_list, count.index % length(var.app_ami_list))
+  instance_type = element(var.app_instance_types, count.index % length(var.app_instance_types))
+  subnet_id     = element(var.private_subnet_ids, count.index % length(var.private_subnet_ids))
+  vpc_security_group_ids = element(var.app_sg_ids, count.index % length(var.app_sg_ids))
   tags = merge(var.tags, {
-    Name = "${var.name_prefix}-app-${count.index + 1}",
+    Name = element(var.app_instance_names, count.index),
     Role = "app"
   })
 }
