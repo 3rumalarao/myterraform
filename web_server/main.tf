be8ff07cb0bdf27eee6f54615dc34b91c45f2d0a
3rumalarao <3rumalarao@gmail.com>
2025-03-30 22:08:10 -0500

    Update main.tf

--- web_server/main.tf
+++ web_server/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web_server" {
-  count                 = var.web_instance_count
-  ami                   = var.web_ami
-  instance_type         = var.web_instance_type
-  subnet_id             = element(var.private_subnet_ids, count.index % length(var.private_subnet_ids))
-  vpc_security_group_ids = [var.web_sg_id]
+  count         = var.web_instance_count
+  ami           = element(var.web_ami_list, count.index % length(var.web_ami_list))
+  instance_type = element(var.web_instance_types, count.index % length(var.web_instance_types))
+  subnet_id     = element(var.private_subnet_ids, count.index % length(var.private_subnet_ids))
+  vpc_security_group_ids = element(var.web_sg_ids, count.index % length(var.web_sg_ids))
   tags = merge(var.tags, {
-    Name = "${var.name_prefix}-web-${count.index + 1}",
+    Name = element(var.web_instance_names, count.index),
     Role = "web"
   })
 }
